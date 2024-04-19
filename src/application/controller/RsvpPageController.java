7123acb9ec1c554e391c353826052da4e2409c3b
steinLH <148312124+steinLH@users.noreply.github.com>
2024-04-19 15:54:04 -0700

    Update RsvpPageController.java

--- src/application/controller/RsvpPageController.java
+++ src/application/controller/RsvpPageController.java
@@ -1,26 +1,50 @@
 package application.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
+import application.Event;
 import application.Main;
+import application.UserProfile;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class RsvpPageController {
 		
 	@FXML
 	private Button homeButton;
-	
 	@FXML
 	private Button myEventButton;
+	@FXML
+	private TextField usernameField;
+	@FXML
+	private TextField locationField;
+	@FXML
+	private TextField eventDateField;
+	@FXML
+	private TextField eventTimeField;
+	
+	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
 	
     @FXML
     private void initialize() {
+    	//System.out.println("usernameField:111 " + usernameField.textProperty().toString());
 
     }
 
@@ -50,7 +74,17 @@ public class RsvpPageController {
 		}
     
     }
-	public void handleRsvp(ActionEvent event) throws Exception {
+	public void handleRsvp(ActionEvent eventIn) throws Exception {
+		
+		System.out.println("myevent  -> myevent ");
+				
+        Connection connection = null;
+        List<Event> eventList = new ArrayList<>(); 
+        Event eventFilter = new Event();
+        Event event = new Event();
+        eventList = event.getEventList(connection,eventFilter);
+        System.out.println(" myevent  -> getEventList "+eventList);
+        
     	// Load the event page
 	    FXMLLoader loader = new FXMLLoader(Main.class.getResource("fxml/rsvpPage.fxml"));
 	    loader.setController(new RsvpPageController());
@@ -128,9 +162,38 @@ public class RsvpPageController {
 		}
     
     }
-	public void handleSaveLaunchEvent(ActionEvent event) throws Exception {
+	public void handleSaveLaunchEvent(ActionEvent acEvent) throws Exception, ParseException, ClassNotFoundException, SQLException {
+		
+        Connection connection = null;
+		
+		// usernameField locationField eventDateField eventTimeField
+		System.out.println("FXML Field:111 ");
+		//initial before update/create event
+		if(usernameField != null && locationField!= null && eventDateField != null && eventTimeField!= null ) {
+			System.out.println("usernameField:  " + usernameField.getText());
+		}
+
+		
+
+		//update/create event
+		System.out.println("RsvpPageController:  updateEvent");
+
+		Event event = new Event(usernameField.getText(),locationField.getText(),dateFormat.parse("2024-06-15 00:00:00"),Time.valueOf("11:30:00"),Time.valueOf("15:30:00"));
+		event.eventId = 6;
+		event.eventDetail = "Join other dog lovers for a guided hike through beautiful Redwood forests.";
+		UserProfile profile = new UserProfile();
+		profile.name = "Kara";
+		event.updateEvent(connection, event, profile);
+		
+		
+        // update/create event successful alert
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Save Successful");
+        alert.setHeaderText("Thank you for successfully launched Event! ");
+        alert.showAndWait();
+        
     	// Load the event page
-	    FXMLLoader loader = new FXMLLoader(Main.class.getResource("fxml/launchPage.fxml"));
+	    FXMLLoader loader = new FXMLLoader(Main.class.getResource("fxml/rsvpPage.fxml"));
 	    loader.setController(new RsvpPageController());
 	    Parent root;
 		try {
@@ -141,7 +204,7 @@ public class RsvpPageController {
  		    // Set the stage with the home scene
  		    Stage stage = new Stage();
  		    stage.setScene(scene);
- 		    stage.setTitle("Launch Page");
+ 		    stage.setTitle("rsvp Page");
  		    stage.show();
  		    
  	        // Close the current window
@@ -150,7 +213,13 @@ public class RsvpPageController {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}finally {
+            try {
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     
     }
 	public void handleCancelLaunch(ActionEvent event) throws Exception {
@@ -197,5 +266,6 @@ public class RsvpPageController {
 	    Stage currentStage = (Stage) homeButton.getScene().getWindow();
 	    currentStage.close();
 		}
+	
 }
 
